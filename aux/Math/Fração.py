2a025f4413306b817effe1b56a4be6ae2b7ac044
Caio Uehara <caioueharamartins@gmail.com>
2023-08-24 23:16:47 -0300

    Aux module: chegar se o denominador é zero

--- aux/Math/Fração.py
+++ aux/Math/Fração.py
@@ -4,13 +4,17 @@ class Fração:
         self.denominador = denominador
     
     def __add__(FracaoA, FracaoB):
+        if(FracaoA.denominador * FracaoB.denominador == 0): return
         return ((FracaoA.numerador * FracaoB.denominador) + (FracaoA.denominador * FracaoB.numerador))/ (FracaoA.denominador * FracaoB.denominador)
 
     def __sub__(FracaoA, FracaoB):
+        if(FracaoA.denominador * FracaoB.denominador == 0): return
         return ((FracaoA.numerador * FracaoB.denominador) - (FracaoA.denominador * FracaoB.numerador))/ (FracaoA.denominador * FracaoB.denominador)
 
     def __mul__(FracaoA, FracaoB):
+        if(FracaoA.denominador * FracaoB.denominador == 0): return
         return (FracaoA.numerador * FracaoB.numerador) / (FracaoA.denominador * FracaoB.denominador)
 
     def __truediv__(FracaoA, FracaoB):
+        if(FracaoA.denominador * FracaoB.denominador == 0): return
         return (FracaoA.numerador * FracaoB.denominador) / (FracaoA.denominador * FracaoB.numerador)
