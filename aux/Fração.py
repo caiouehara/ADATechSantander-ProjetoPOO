7c3ea4d2fecafffb20e7558930cf145993f3e965
Caio Uehara <caioueharamartins@gmail.com>
2023-08-24 21:24:07 -0300

    Aux module: add more methods in Fracao

--- aux/Fração.py
+++ aux/Fração.py
@@ -6,3 +6,11 @@ class Fração:
     def __add__(FracaoA, FracaoB):
         return ((FracaoA.numerador * FracaoB.denominador) + (FracaoA.denominador * FracaoB.numerador))/ (FracaoA.denominador * FracaoB.denominador)
 
+    def __sub__(FracaoA, FracaoB):
+        return ((FracaoA.numerador * FracaoB.denominador) - (FracaoA.denominador * FracaoB.numerador))/ (FracaoA.denominador * FracaoB.denominador)
+
+    def __mul__(FracaoA, FracaoB):
+        return (FracaoA.numerador * FracaoB.numerador) / (FracaoA.denominador * FracaoB.denominador)
+
+    def __truediv__(FracaoA, FracaoB):
+        return (FracaoA.numerador * FracaoB.denominador) / (FracaoA.denominador * FracaoB.numerador)
